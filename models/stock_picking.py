8009dcf3e8f81266eec3273d8241f2590f12ca0a
Tilo <tilo.karczewski@gmx>
2022-04-25 15:24:35 +0200

    lauffähiges parsen der csv und prüfung, ob scancode gültig

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -1,10 +1,27 @@
 from odoo import api, fields, models
+import csv, base64
 
 class Picking(models.Model):
     _inherit = 'stock.picking'
 
     csv_file = fields.Binary(string='CSV File')
+    output = fields.Char(string='internal output')
 
-    @api.onchange('csv_file')
-    def parse_csv(self):
-        return {'warning': {'title': "Output from parser", 'message': "Hallo Welt"},}
+    def ebom_csv(self):
+        raw = base64.b64decode(self.csv_file)
+        raw_list = raw.split(b'\n')
+        str_list = []
+        for row in raw_list:
+            str_row = row.decode(encoding='utf-8', errors='ignore').split(';')
+            str_list.append(str_row)
+
+        output = ""
+        for row in str_list:
+            result = self.env['product.template'].search([('default_code', '=', row[2])])
+            if len(result) > 0:
+                output = output + "1-"
+            else:
+                output = output + "0-"
+        self.output = output
+
+        return True
