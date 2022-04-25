fbf6df69acc844447732924e0497bddd3e6abdc6
Tilo <tilo.karczewski@gmx>
2022-04-25 20:34:40 +0200

    erstes erfolgreiches line create, übernahme der attribute aus stock.picking noch vervollständigen

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -11,17 +11,28 @@ class Picking(models.Model):
         raw = base64.b64decode(self.csv_file)
         raw_list = raw.split(b'\n')
         str_list = []
+        offset = 0
         for row in raw_list:
-            str_row = row.decode(encoding='utf-8', errors='ignore').split(';')
-            str_list.append(str_row)
+            if offset < 1:
+                str_row = row.decode(encoding='utf-8', errors='ignore').split(';')
+                str_list.append(str_row)
+            offset = offset + 1
 
         output = ""
         for row in str_list:
             result = self.env['product.template'].search([('default_code', '=', row[2])])
             if len(result) > 0:
-                output = output + "1-"
+                output = output + str(result[0].id) + "-"
+
+                self.env['stock.move'].create({
+                    'product_id': result[0].id,
+                    'product_qty': int(row[0]),
+                    'picking_id': self.id,
+                    'reference': self.name
+                })
             else:
                 output = output + "0-"
         self.output = output
+        #self.env.cr.commit()
 
         return True
