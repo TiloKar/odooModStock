d878ef159da58a6341113e21d30eabb0578d32d9
Tilo <tilo.karczewski@gmx>
2022-07-01 17:19:26 +0200

    import kommilager für bestände vorbereitet, noch abzuschließen. script sammelt fehler und gibt diese in csv zurück

--- models/bbi_stock_location.py
+++ models/bbi_stock_location.py
@@ -1,6 +1,7 @@
 from odoo import api, fields, models
-import csv, base64, sys, xlrd, datetime
+import csv, base64, sys, xlrd, xlwt, datetime
 from odoo.exceptions import ValidationError
+from io import BytesIO, StringIO
 
 class BbiStockLocation(models.Model):
     _name = "bbi.stock.location"
@@ -56,7 +57,9 @@ class BbiStockLocation(models.Model):
                 hit[0].update(d)
             else:
                 self.env['stock.quant'].create(d)
-
+    #einmalige übernahme der Kommilager aus der parseLagerliste
+    # für jedes komilager eine geschlossen MO für Bestände
+    # und eine offene MO für noch nicht gedeckte bedarfe
     def parseKommilager(self):
         try:
             raw = base64.decodestring(self.myFile) #vorbereitung binary
@@ -73,92 +76,104 @@ class BbiStockLocation(models.Model):
 
         datasetsBedarf = [] # wird ein array mit den anzuhängenden dictionaries
         datasetsBestand = [] # wird ein array mit den anzuhängenden dictionaries
-        for i in range(sheet.nrows):
-            if i < 1:
+        scancodeSetsError =[]
+        for k in range(book.nsheets):
+            if k < 1:
                 continue
-            if isinstance(sheet.cell(i, 0).value, str): #fallunterscheidung für als zahl interpretierte scancodes
-                rowCode = str(sheet.cell(i, 0).value).replace('\n','')
-            else:
-                rowCode = str(int(sheet.cell(i, 0).value))
-
-            result = self.env['product.product'].search([('default_code', '=', rowCode)]) # product_template id.ermitteln
-            if len(result) == 0:
-                raise ValidationError('Scancode: {} in Zeile {} nicht gefunden'.format(rowCode, i+1))
-
-            if result[0].product_tmpl_id.type == 'product':
-                if isinstance(sheet.cell(i, 6).value, str):
-                    bestand = 0
+            sheet = book.sheets()[k]
+            for i in range(sheet.nrows):
+                if i < 1:
+                    continue
+                if isinstance(sheet.cell(i, 0).value, str): #fallunterscheidung für als zahl interpretierte scancodes
+                    rowCode = str(sheet.cell(i, 0).value).replace('\n','')
                 else:
-                    bestand = int(sheet.cell(i, 6).value)
+                    rowCode = str(int(sheet.cell(i, 0).value))
 
-                if isinstance(sheet.cell(i, 7).value, str):
-                    bedarf = 0
+                result = self.env['product.product'].search([('default_code', '=', rowCode)]) # product_template id.ermitteln
+                if len(result) == 0:
+                    scancodeSetsError.append({'origin' : 'Scancode: {} in {} Zeile {} nicht gefunden'.format(rowCode, i+1, sheet.name),'scancode' : rowCode});
+                    #raise ValidationError('Scancode: {} in Zeile {} nicht gefunden'.format(rowCode, i+1))
                 else:
-                    bedarf = int(sheet.cell(i, 7).value)
-                print('abgeschlossen Projekt bom -- {}  -- product_product: {} mit bestand {} und bedarf {} aufgenommen'.format(sheet.name,result[0],bestand,bedarf))
-                #to do Los ID
-                if (bedarf - bestand) > 0:
-                    datasetsBedarf.append({
-                        'product_id': result[0].id,
-                        'product_uom_id': result[0].uom_id.id,
-                        'product_uom_qty': bedarf - bestand,
-                    })
-                elif bestand > 0:
-                    datasetsBestand.append({
-                        'product_id': result[0].id,
-                        'product_uom_qty': bestand,
-                        'product_uom_id': result[0].uom_id.id,
-                    })
+                    if (result[0].product_tmpl_id.type == 'product') and ( result[0].product_tmpl_id.detailed_type == 'product') : # nur zählbare erfassen
+                        if isinstance(sheet.cell(i, 6).value, str):
+                            bestand = 0
+                        else:
+                            bestand = int(sheet.cell(i, 6).value)
 
-        #hilfsproduct für MO
-        newProduct = self.env['product.product'].create({
-            'name': "Projekt-{}-Hilfsprodukt".format(sheet.name),
-            'detailed_type': "product",
-            'type': "product",
-            'purchase_ok': False,
-            'active': False,
-        })
-
-        #Kopfdaten
-        prodName = "TagX-BOM-{}-offene Bedarfe".format(sheet.name)
-        newProduction = self.env['mrp.production'].create({
-            'name': prodName,
-            'origin': "Terminal",
-            'product_id': newProduct.id,
-            'product_uom_id': newProduct.uom_id.id,
-            'product_qty' : 1,
-            'product_uom_qty' : 1,
-        })
-        #move to stock
-        self.env['stock.move'].create({
-            'name': prodName,
-            'origin': prodName,
-            'product_id': newProduct.id,
-            'production_id': newProduction.id,
-            'product_uom': newProduct.uom_id.id,
-            'product_uom_qty' : 1,
-            'location_id': 15,
-            'location_dest_id': 8,
-            'warehouse_id': 1,
-            'sequence' : 10,
-        })
-
-        #move from stock to production
-        sequence = 0
-        for d in datasetsBedarf:
-            sequence += 1
-            #if sequence == 10 : sequence = 11
-            self.env['stock.move'].create({
-                'name': prodName,
-                'origin': prodName,
-                'product_id': d['product_id'],
-                'raw_material_production_id': newProduction.id,
-                'product_uom': d['product_uom_id'],
-                'product_uom_qty' : d['product_uom_qty'],
-                'location_id': 8,
-                'location_dest_id': 15,
-                'warehouse_id': 1,
+                        if isinstance(sheet.cell(i, 7).value, str):
+                            bedarf = 0
+                        else:
+                            bedarf = int(sheet.cell(i, 7).value)
+                        print('abgeschlossen Projekt bom -- {}  -- product_product: {} mit bestand {} und bedarf {} aufgenommen'.format(sheet.name,result[0],bestand,bedarf))
+                        #to do Los ID
+                        if (bedarf - bestand) > 0:
+                            datasetsBedarf.append({
+                                'product_id': result[0].id,
+                                'product_uom_id': result[0].uom_id.id,
+                                'product_uom_qty': bedarf - bestand,
+                            })
+                        elif bestand > 0:
+                            datasetsBestand.append({
+                                'product_id': result[0].id,
+                                'product_uom_qty': bestand,
+                                'product_uom_id': result[0].uom_id.id,
+                            })
+        if len(scancodeSetsError) == 0 :
+                #hilfsproduct für MO Bestand
+            newProduct = self.env['product.product'].create({
+                'name': "{} TagX Hilfsprodukt für Bestand aus Terminal".format(sheet.name),
+                'detailed_type': "product",
+                'type': "product",
+                'purchase_ok': False,
+                'active': False,
             })
+
+            #Kopfdaten MO Bestand
+            newProduction = self.env['mrp.production'].create({
+                'origin': "{} TagX Terminal Bestände".format(sheet.name),
+                'product_id': newProduct.id,
+                'product_uom_id': newProduct.uom_id.id,
+                'product_qty' : 1,
+                'product_uom_qty' : 1,
+            })
+            #move to stock
+            self.env['stock.move'].create({
+                'name': newProduction.name,
+                'origin': newProduction.name,
+                'reference': newProduction.name,
+                'product_id': newProduct.id,
+                'production_id': newProduction.id,
+                'product_uom': newProduct.uom_id.id,
+                'product_uom_qty' : 1,
+                'location_id': 15,
+                'location_dest_id': 8,
+                'warehouse_id': 1,
+                'sequence' : 10,
+            })
+
+            #move from stock to production
+            for d in datasetsBedarf:
+                self.env['stock.move'].create({
+                    'name': newProduct,
+                    'origin': newProduct,
+                    'reference': newProduction.name,
+                    'product_id': d['product_id'],
+                    'raw_material_production_id': newProduction.id,
+                    'product_uom': d['product_uom_id'],
+                    'product_uom_qty' : d['product_uom_qty'],
+                    'location_id': 8,
+                    'location_dest_id': 15,
+                    'warehouse_id': 1,
+                    'sequence' : 1,
+                })
+        else:
+            ausgabe = ''
+            for d in scancodeSetsError:
+                ausgabe+= "{};{}\n".format(d['scancode'],d['origin'])
+            raw = ausgabe.encode(encoding='cp1252', errors='replace') # String encoden
+            self.myFile = base64.b64encode(raw) # binärcode mit b64 encoden
+            self.myFile_file_name = 'errors.csv' # Name und Format des Downloads
+
 
     #einmalige übernahme des wareneingangs buchs an TagX
     #nur offene etsellungen werden übernommen und auch nicht bestätigt,
