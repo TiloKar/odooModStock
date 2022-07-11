0ba8fac16054dca54fffa6bc8d46aa8d2f51f79d
Tilo <tilo.karczewski@gmx>
2022-07-11 22:06:59 +0200

    erstes lauffähiges quants und nachtrag raise bypass stock_quant reservierung

--- models/bbi_tagx_lagerliste.py
+++ models/bbi_tagx_lagerliste.py
@@ -57,9 +57,8 @@ class BbiStockLocation(models.Model):
                 notFound.append({
                     'name': str(sheet.cell(i, 1).value),
                     'default_code' : rowCode,
-                    'bbiDrawingNb' : str(sheet.cell(i, 2).value),
                 })
-                print('{} kein treffer 1905 -- code: {}'.format(i+1,rowCode))
+                print('{} in 1905 nicht gefunden -- code: {}'.format(i+1,rowCode))
             else:
                 if (result[0].product_tmpl_id.type == 'product') and ( result[0].product_tmpl_id.detailed_type == 'product') : # nur zählbare erfassen
                     if isinstance(sheet.cell(i, 6).value, str):
@@ -78,15 +77,14 @@ class BbiStockLocation(models.Model):
                         'product_uom_id': result[0].uom_id.id,
                         'default_code' : rowCode,
                     })
-                    print('{} product_product: {} mit bestand1905: {} und bestandOdoo: {} aufgenommen'.format(i+1,rowCode,verbrauch,summeOdoo))
+                    print('{} in 1905 , product: {} mit bestand1905: {} und bestandOdoo: {} aufgenommen'.format(i+1,rowCode,verbrauch,summeOdoo))
                     #if summeOdoo > 0: break # debug
                 else: #roter rotePunkt
                     rotePunkteOdoo.append({
                         'name': str(sheet.cell(i, 1).value),
                         'default_code' : rowCode,
-                        'bbiDrawingNb' : str(sheet.cell(i, 2).value),
                     })
-                    print('{} Verbrauchsartikel 1905 -- code: {} -- name: {}'.format(i+1,rowCode,str(sheet.cell(i, 1).value)))
+                    print('{} in 1905 Verbrauchsartikel -- code: {} -- name: {}'.format(i+1,rowCode,str(sheet.cell(i, 1).value)))
 
 
         # debug abbruch
@@ -100,8 +98,7 @@ class BbiStockLocation(models.Model):
         #self.myFile_file_name = 'products_checked_or_generated_for quants.csv' # Name und Format des Downloads
         #return
 
-
-
+        #ende for 1905
 
         datasetsBestand = [] # wird ein array mit den anzuhängenden dictionaries
         sheet = book.sheets()[0]
@@ -120,7 +117,6 @@ class BbiStockLocation(models.Model):
                 notFound.append({
                     'name': str(sheet.cell(i, 1).value),
                     'default_code' : rowCode,
-                    'bbiDrawingNb' : str(sheet.cell(i, 2).value),
                 })
                 print('{} kein treffer -- code: {} -- name: {}'.format(i+1,rowCode,str(sheet.cell(i, 1).value)))
             else:
@@ -130,62 +126,81 @@ class BbiStockLocation(models.Model):
                     else:
                         bestand = float(sheet.cell(i, 8).value)
 
-                    #schauene ob es zusätzliche menge in 1105 gibt, dann differenz in richtung 1905 dazu packen
-                    if (len(datasetsBestand1905) > 0):
-                        for d in datasetsBestand1905:
-                            if d['product_id'] == result[0].id:
-                                dif = d['verbrauch1905'] - d['verbrauchOdoo']
-                                if dif > 0:
-                                    new = differenz + bestand
-                                    print("correcting from 1906: id {} from {} to {}".format(d['product_id'],bestand,new))
-                                    bestand = new
-                                break
-
-                    datasetsBestand.append({'product_id': result[0].id,
+                    vals = {'product_id': result[0].id,
                         'name': result[0].name,
                         'product_uom_qty': bestand,
                         'product_uom_id': result[0].uom_id.id,
-                        'default_code' : rowCode})
+                        'default_code' : rowCode}
 
-                    print('{} product_product: {} mit bestand {} aufgenommen'.format(i+1,result[0],bestand))
+
+                    #schauene ob es zusätzliche menge in 1105 gibt, dann differenz in richtung 1905 dazu packen
+                    res1905 = list(filter(lambda d: d['product_id'] == result[0].id,datasetsBestand1905))
+                    if (len(res1905) > 0):
+                        vals['verbrauch1905'] = res1905[0]['verbrauch1905']
+                        vals['verbrauchOdoo'] = res1905[0]['verbrauchOdoo']
+
+                    datasetsBestand.append(vals)
+                    # debug 
+                    break
+                    print('{} in Lagerliste product: {} mit bestand {} aufgenommen'.format(i+1,result[0].default_code,bestand))
 
                 else: #roter rotePunkt
                     rotePunkteOdoo.append({
                         'name': str(sheet.cell(i, 1).value),
                         'default_code' : rowCode,
-                        'bbiDrawingNb' : str(sheet.cell(i, 2).value),
                     })
-                    print('{} Verbrauchsartikel -- code: {} -- name: {}'.format(i+1,rowCode,str(sheet.cell(i, 1).value)))
+                    print('{} in Lagerliste Verbrauchsartikel -- code: {} -- name: {}'.format(i+1,rowCode,str(sheet.cell(i, 1).value)))
+
         #ende for über alle zeilen im blatt lagerliste
 
         #ab hier MO für bestand
         if (len(datasetsBestand) > 0) and (generate == True) :
             print("generating quant {}".format(sheet.name))
+            for d in datasetsBestand:
+                dif = 0
+                toSet = d['product_uom_qty']
+                if d['verbrauch1905']:
+                    dif = d['verbrauch1905'] - d['verbrauchOdoo']
+                if dif > 0:
+                    toSet = dif + d['product_uom_qty']
 
+                thisProduct = self.env['product.product'].search([('id','=',d['product_id'])])
+                thisProduct._compute_quantities()
+                ist = thisProduct.qty_available
 
-            #hilfsproduct für MO Bestand
-            #newQuant = self.env['stock.quant'].create({
-            #    'name': "{} TagX Hilfsprodukt für Bestand aus Terminal".format(sheet.name),
-            #    'detailed_type': "product",
-            #    'type': "product",
-            #    'purchase_ok': False,
-            #    'active': False,
-            #})
+                newQuant = self.env['stock.quant'].create({
+                    'product_id': d['product_id'],
+                    'location_id': 9,
+                    'inventory_quantity': toSet,
+                #    'type': "product",
+                #    'purchase_ok': False,
+                #    'active': False,
+                })
+                newQuant.action_apply_inventory()
 
             #newProduction.action_confirm()
 
             #newProduction.button_mark_done()
 
-        ausgabe = 'Anzahl;code;name\nto create:\n'
+        ausgabe = 'Anzahl;code;name;Anzahl 1905;Anzahl Odoo MO;\nto create:\n'
         #    datasetsBestand.append({
         #        'product_id': result[0].id,
+        #        'name': result[0].name,
         #        'product_uom_qty': bestand,
         #        'product_uom_id': result[0].uom_id.id,
         #        'default_code' : rowCode,
+        #                        vals['verbrauch1905'] = result[0]['verbrauch1905']
+        #                        vals['verbrauchOdoo'] = result[0]['verbrauchOdoo']
         #    })
         if len(datasetsBestand) > 0 :
             for d in datasetsBestand:
-                ausgabe+= "{};{};{}\n".format(d['product_uom_qty'],d['default_code'],d['name'])
+                dif = 0
+                if d['verbrauch1905']:
+                    dif = d['verbrauch1905'] - d['verbrauchOdoo']
+                if dif > 0:
+                    ausgabe+= "{};{};{};{};{}\n".format(dif + d['product_uom_qty'],d['default_code'],d['name'],d['verbrauch1905'],d['verbrauchOdoo'])
+                else:
+                    ausgabe+= "{};{};{}\n".format(d['product_uom_qty'],d['default_code'],d['name'])
         #notFound.append({
         #    'name': str(sheet.cell(i, 1).value),
         #    'default_code' : rowCode,
--- models/uom_tagx.py
+++ models/uom_tagx.py
@@ -22,6 +22,60 @@ class StockQuant(models.Model):
                 print("bypassing raise in check_location_id")
                 #raise ValidationError(_('You cannot take products from or deliver products to a location of type "view" (%s).') % quant.location_id.name)
 
+    #temporäres ausserkraftsetzen der raise
+    @api.model
+    def _update_reserved_quantity(self, product_id, location_id, quantity, lot_id=None, package_id=None, owner_id=None, strict=False):
+        """ Increase the reserved quantity, i.e. increase `reserved_quantity` for the set of quants
+        sharing the combination of `product_id, location_id` if `strict` is set to False or sharing
+        the *exact same characteristics* otherwise. Typically, this method is called when reserving
+        a move or updating a reserved move line. When reserving a chained move, the strict flag
+        should be enabled (to reserve exactly what was brought). When the move is MTS,it could take
+        anything from the stock, so we disable the flag. When editing a move line, we naturally
+        enable the flag, to reflect the reservation according to the edition.
+        :return: a list of tuples (quant, quantity_reserved) showing on which quant the reservation
+            was done and how much the system was able to reserve on it
+        """
+        self = self.sudo()
+        rounding = product_id.uom_id.rounding
+        quants = self._gather(product_id, location_id, lot_id=lot_id, package_id=package_id, owner_id=owner_id, strict=strict)
+        reserved_quants = []
+
+        if float_compare(quantity, 0, precision_rounding=rounding) > 0:
+            # if we want to reserve
+            available_quantity = sum(quants.filtered(lambda q: float_compare(q.quantity, 0, precision_rounding=rounding) > 0).mapped('quantity')) - sum(quants.mapped('reserved_quantity'))
+            if float_compare(quantity, available_quantity, precision_rounding=rounding) > 0:
+                print("bypassing raise in _update_reserved_quantity")
+                #raise UserError(_('It is not possible to reserve more products of %s than you have in stock.', product_id.display_name))
+        elif float_compare(quantity, 0, precision_rounding=rounding) < 0:
+            # if we want to unreserve
+            available_quantity = sum(quants.mapped('reserved_quantity'))
+            if float_compare(abs(quantity), available_quantity, precision_rounding=rounding) > 0:
+                print("bypassing raise in _update_reserved_quantity")
+                #raise UserError(_('It is not possible to unreserve more products of %s than you have in stock.', product_id.display_name))
+        else:
+            return reserved_quants
+
+        for quant in quants:
+            if float_compare(quantity, 0, precision_rounding=rounding) > 0:
+                max_quantity_on_quant = quant.quantity - quant.reserved_quantity
+                if float_compare(max_quantity_on_quant, 0, precision_rounding=rounding) <= 0:
+                    continue
+                max_quantity_on_quant = min(max_quantity_on_quant, quantity)
+                quant.reserved_quantity += max_quantity_on_quant
+                reserved_quants.append((quant, max_quantity_on_quant))
+                quantity -= max_quantity_on_quant
+                available_quantity -= max_quantity_on_quant
+            else:
+                max_quantity_on_quant = min(quant.reserved_quantity, abs(quantity))
+                quant.reserved_quantity -= max_quantity_on_quant
+                reserved_quants.append((quant, -max_quantity_on_quant))
+                quantity += max_quantity_on_quant
+                available_quantity += max_quantity_on_quant
+
+            if float_is_zero(quantity, precision_rounding=rounding) or float_is_zero(available_quantity, precision_rounding=rounding):
+                break
+        return reserved_quants
+
 
 class StockMove(models.Model):
     _inherit = 'stock.move'
