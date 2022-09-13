dc566c948bf5456874372d41a0ebb686e603d141
hnn@bbi-biotech.com <hnn@bbi-biotech.com>
2022-09-14 01:19:59 +0200

    weitere Versuche und fix an product_template

--- models/bbi_replenishment.py
+++ models/bbi_replenishment.py
@@ -125,8 +125,8 @@ FROM (SELECT
         q.company_id,
         wh.id as warehouse_id
     FROM
-        GENERATE_SERIES((now() at time zone 'utc')::date - interval '24month',
-        (now() at time zone 'utc')::date + interval '24 month', '1 day'::interval) date,
+        GENERATE_SERIES((now() at time zone 'utc')::date - interval '48month',
+        (now() at time zone 'utc')::date + interval '48 month', '1 day'::interval) date,
         stock_quant q
     LEFT JOIN stock_location l on (l.id=q.location_id)
     LEFT JOIN stock_warehouse wh ON l.parent_path like concat('%/', wh.view_location_id, '/%')
@@ -142,11 +142,11 @@ FROM (SELECT
         'forecast' as state,
         GENERATE_SERIES(
         CASE
-            WHEN m.state = 'done' THEN (now() at time zone 'utc')::date - interval '24month'
+            WHEN m.state = 'done' THEN (now() at time zone 'utc')::date - interval '48month'
             ELSE m.date::date
         END,
         CASE
-            WHEN m.state != 'done' THEN (now() at time zone 'utc')::date + interval '24 month'
+            WHEN m.state != 'done' THEN (now() at time zone 'utc')::date + interval '48 month'
             ELSE m.date::date - interval '1 day'
         END, '1 day'::interval)::date date,
         CASE
@@ -173,18 +173,17 @@ GROUP BY product_id, product_tmpl_id, state, date, company_id, warehouse_id
 class StockWarehouseOrderpoint(models.Model):
     _inherit = "stock.warehouse.orderpoint"
 
-
+    # Hilfsmethode existiert um am jeden Ausgang der Methode _get_orderpoint_action() zusätzlich noch die gespeicherten Supplier- und Vendorinformationen einzufügen
     def restore_orderpoint_info(self,supInfos):
         action = self.env["ir.actions.actions"]._for_xml_id("stock.action_orderpoint_replenish")
         action['context'] = self.env.context
         orderpoints = self.env['stock.warehouse.orderpoint'].search([])
+        print("Marker 11")
         for op in orderpoints:
             for item in supInfos:
-                #print(op.product_id.id)
                 if op.product_id.id == item['product_id']:
                     op.supplier_id = item['supplier_id']
                     op.vendor_id = item['vendor_id']
-
         return action
 
     def _get_orderpoint_action(self):
@@ -203,18 +202,27 @@ class StockWarehouseOrderpoint(models.Model):
         # It means that when there will be a archived orderpoint on a location + product, the replenishment
         # report won't take in account this location + product and it won't create any manual orderpoint
         # In master: the active field should be remove
-        #todo snoozed behandeln ???
+
+        # Alle löschen, die manuell zu bestellen sind, ein Schlummerdatum eingestellt haben bzw. das Schlummerdatum nicht heute ist.
+        #to_delete = self.env['stock.warehouse.orderpoint'].search(['&', ('trigger','=','manual'),  '|', ('snoozed_until','=',False), ('snoozed_until','=', fields.date.today())])
         to_delete = self.env['stock.warehouse.orderpoint'].search([('trigger','=','manual')])
         std_supplier = []
 
+
+
         #workaroud löschen aller orderpoints und apspeichern der supinfo
+        print("Marker 1")
         for op in to_delete:
-            #print(op)
+            if (op.qty_forecast * (-1)) == op.qty_to_order:
+                to_delete = to_delete - op
+                continue
+            print("hier")
             if op.supplier_id:
                 std_supplier.append({'supplier_id':op.supplier_id.id,'product_id':op.product_id.id,'vendor_id':op.vendor_id.id,})
-            op.unlink()
+        print(to_delete.product_id.product_tmpl_id.name)
+        to_delete.unlink()
         orderpoints = self.env['stock.warehouse.orderpoint'].with_context(active_test=False).search([])
-
+        print("Marker 2")
         # Remove previous automatically created orderpoint that has been refilled.
         orderpoints_removed = orderpoints._unlink_processed_orderpoints()
         orderpoints = orderpoints - orderpoints_removed - to_delete
@@ -224,22 +232,18 @@ class StockWarehouseOrderpoint(models.Model):
         # Take 3 months since it's the max for the forecast report
         # Das bedeutet, wenn man den Forcasted Report ausgibt werden nur Produkte angezeigt, deren Bedarfmeldungen innerhalb von 3 Monaten liegen.
         # Ein MO die heute (31.08.) eröffnet wird, wird noch angezeigt, wenn das geplannte Datum am 30.11. ist, aber verschwindet, wenn das Datum der 1.12 ist.
-        # EDIT: Hanning Liu months von 3 auf 24 gesetzt um die Leadtage zu erhöhen
-        to_date = add(fields.date.today(), months=24)
+        # EDIT: Hanning Liu months von 3 auf 48 gesetzt um die Leadtage zu erhöhen
+        to_date = add(fields.date.today(), months=48)
         # report.story.quantity zeigt den forcasted reported zu allen Produkten an und ist somit eine Erweiterung zum stock.quant
 
         qty_by_product_warehouse = self.env['bbi.report.stock.quantity'].read_group(
             #edited tk: auch hier den filter auf date komplett entfernt
-            [('state', '=', 'forecast')],
+            #[('state', '=', 'forecast')],
+            [('date', '=', to_date), ('state', '=', 'forecast')],
             ['product_id', 'product_qty', 'warehouse_id'],
             ['product_id', 'warehouse_id'], lazy=False)
 
-        for item in qty_by_product_warehouse:
-            if item['product_id'][0] == 77776:
-                print(item['product_id'])
-                print(item['product_qty'])
-
-
+        print("Marker 3")
 
         for group in qty_by_product_warehouse:
             #setzt Warehouse id Stock = 1 Inventar = 2
@@ -253,6 +257,7 @@ class StockWarehouseOrderpoint(models.Model):
             # to refill = alle aufzufüllenden Produkte mit dem Format (73628, 1): -5.0 / nur negative Product_qty durch vorherige Abfrage
             to_refill[(group['product_id'][0], warehouse_id)] = group['product_qty']
         # wenn keine Produkte zum auffüllen vorhanden sind bricht die Methode hier ab
+        print("Marker 4")
 
         if not to_refill:
             return self.restore_orderpoint_info(std_supplier)
@@ -276,7 +281,8 @@ class StockWarehouseOrderpoint(models.Model):
             # EDIT Hanning Liu: Wenn keine Leaddays in der Supplierinfo eingetragen wird, wird derzeit standardmäßig eine Leadtime von 0 eingetragen
             # Somit wird, sobald die Leaddays 0 betragen automatisch eine 365 eingetragen
             # Das ist wichtig für die Fertigung, die lediglich einen Bedarf eintragen, aber keinen Lieferanten, wodurch eine Leadtime von 0 entsteht und somit nicht in der Auffüllung angezeigt wird.
-            #if lead_days == 0:
+            #lead_days = lead_days+1000
+            #print(lead_days)
             #    lead_days = 365
             #print("Produkt: " + str(product.product_tmpl_id.name) + " Lead_days: " +str(lead_days))
             #pwh_per_day ist ein Gruppierung der Produkte nach den Leadtagen. D.h. 10 Leadtage --> xxx, xxx, xxx PRoduct
@@ -284,22 +290,23 @@ class StockWarehouseOrderpoint(models.Model):
             pwh_per_day[(lead_days, warehouse)].append(product.id)
 
         #print (str(pwh_per_day))
-
+        print("Marker 5")
         # group product by lead_days and warehouse in order to read virtual_available
         # in batch
         for (days, warehouse), p_ids in pwh_per_day.items():
             products = self.env['product.product'].browse(p_ids)
             qties = products.with_context(
                 warehouse=warehouse.id,
+                #to_date=fields.datetime.now() + relativedelta.relativedelta(days=days)
                 to_date=fields.datetime.now() + relativedelta.relativedelta(days=1460) # Mod TK: hier statisch alle zugänge über 4 jahre verrechnen
             ).read(['virtual_available'])
 
-            #print(str(qties) + " test")
             for qty in qties:
                 if float_compare(qty['virtual_available'], 0, precision_rounding=product.uom_id.rounding) >= 0:
                     key_to_remove.append((qty['id'], warehouse.id))
                 else:
                     to_refill[(qty['id'], warehouse.id)] = qty['virtual_available']
+        print("Marker 6")
 
         for key in key_to_remove:  #Auffüllung sollte damit nicht mehr auf die lead times achten
             del to_refill[key]
@@ -319,6 +326,7 @@ class StockWarehouseOrderpoint(models.Model):
             (record.get('product_id')[0], record.get('warehouse_id')[0]): record.get('qty_to_order')
             for record in orderpoint_by_product_warehouse
         }
+        print("Marker 7")
         for (product, warehouse), product_qty in to_refill.items():
             qty_in_progress = qty_by_product_wh.get((product, warehouse)) or 0.0
             qty_in_progress += orderpoint_by_product_warehouse.get((product, warehouse), 0.0)
@@ -343,7 +351,7 @@ class StockWarehouseOrderpoint(models.Model):
             (record.get('product_id')[0], record.get('location_id')[0]): record.get('ids')[0]
             for record in orderpoint_by_product_location
         }
-
+        print("Marker 8")
         orderpoint_values_list = []
         for (product, warehouse), product_qty in to_refill.items():
             lot_stock_id = lot_stock_id_by_warehouse[warehouse]
@@ -358,7 +366,7 @@ class StockWarehouseOrderpoint(models.Model):
                     'company_id': self.env['stock.warehouse'].browse(warehouse).company_id.id,
                 })
                 orderpoint_values_list.append(orderpoint_values)
-
+        print("Marker 9")
         orderpoints = self.env['stock.warehouse.orderpoint'].with_user(SUPERUSER_ID).create(orderpoint_values_list)
         for orderpoint in orderpoints:
             orderpoint_wh = orderpoint.location_id.warehouse_id
@@ -366,4 +374,39 @@ class StockWarehouseOrderpoint(models.Model):
             if not orderpoint.route_id:
                 orderpoint._set_default_route_id()
             orderpoint.qty_multiple = orderpoint._get_qty_multiple_to_order()
+        print("Marker 10")
         return self.restore_orderpoint_info(std_supplier)
+
+    @api.depends('rule_ids', 'product_id.seller_ids', 'product_id.seller_ids.delay')
+    def _compute_lead_days(self):
+        for orderpoint in self.with_context(bypass_delay_description=True):
+            if not orderpoint.product_id or not orderpoint.location_id:
+                orderpoint.lead_days_date = False
+                continue
+            values = orderpoint._get_lead_days_values()
+            lead_days, dummy = orderpoint.rule_ids._get_lead_days(orderpoint.product_id, **values)
+            #lead_days_date = fields.Date.today() + relativedelta.relativedelta(days=lead_days)
+            lead_days_date = fields.Date.today() + relativedelta.relativedelta(days=1460) #EDIT HNN
+            orderpoint.lead_days_date = lead_days_date
+
+    @api.depends('qty_multiple', 'qty_forecast', 'product_min_qty', 'product_max_qty')
+    def _compute_qty_to_order(self):
+        for orderpoint in self:
+            if not orderpoint.product_id or not orderpoint.location_id:
+                orderpoint.qty_to_order = False
+                continue
+            qty_to_order = 0.0
+            rounding = orderpoint.product_uom.rounding
+            if float_compare(orderpoint.qty_forecast, orderpoint.product_min_qty, precision_rounding=rounding) < 0:
+                qty_to_order = max(orderpoint.product_min_qty, orderpoint.product_max_qty) - orderpoint.qty_forecast
+
+                remainder = orderpoint.qty_multiple > 0 and qty_to_order % orderpoint.qty_multiple or 0.0
+                if float_compare(remainder, 0.0, precision_rounding=rounding) > 0:
+                    qty_to_order += orderpoint.qty_multiple - remainder
+            orderpoint.qty_to_order = qty_to_order
+            #print ("Orderprodukt: ")
+            #print (orderpoint.product_id.product_tmpl_id.name)
+            #print ("Ordermenge: ")
+            #print (orderpoint.qty_forecast)
+            #print ("Qty_to_order: ")
+            #print (orderpoint.qty_to_order)
