a059778b22b4fcd3cdeb3be95ea26117509cde96
Tilo <tilo.karczewski@gmx>
2022-09-14 17:23:12 +0200

    weiter mit eigener abfrage

--- models/bbi_replenishment.py
+++ models/bbi_replenishment.py
@@ -330,7 +330,6 @@ GROUP BY product_id, product_tmpl_id, state, date, company_id, warehouse_id
 class StockWarehouseOrderpoint(models.Model):
     _inherit = "stock.warehouse.orderpoint"
 
-
     def restore_orderpoint_info(self,supInfos):
         action = self.env["ir.actions.actions"]._for_xml_id("stock.action_orderpoint_replenish")
         action['context'] = self.env.context
@@ -353,144 +352,54 @@ class StockWarehouseOrderpoint(models.Model):
         - It creates the orderpoints for missing quantity that were not refill by an upper option.
         return replenish report ir.actions.act_window
         """
+        #eigentliche Rückgabe zum Aufbau im Frontend, wird durch manipulation auf variable orderpoints im laufeder methode verändert
+        action = self.env["ir.actions.actions"]._for_xml_id("stock.action_orderpoint_replenish")
+        action['context'] = self.env.context
 
-        #action = self.env["ir.actions.actions"]._for_xml_id("stock.action_orderpoint_replenish")
-        #action['context'] = self.env.context
-        # Search also with archived ones to avoid to trigger product_location_check SQL constraints later
-        # It means that when there will be a archived orderpoint on a location + product, the replenishment
-        # report won't take in account this location + product and it won't create any manual orderpoint
-        # In master: the active field should be remove
-        #todo snoozed behandeln ???
-        to_delete = self.env['stock.warehouse.orderpoint'].search([('trigger','=','manual')])
-        std_supplier = []
-
-        #workaroud löschen aller orderpoints und apspeichern der supinfo
-        for op in to_delete:
-            #print(op)
-            if op.supplier_id:
-                std_supplier.append({'supplier_id':op.supplier_id.id,'product_id':op.product_id.id,'vendor_id':op.vendor_id.id,})
-            op.unlink()
         orderpoints = self.env['stock.warehouse.orderpoint'].with_context(active_test=False).search([])
 
-        # Remove previous automatically created orderpoint that has been refilled.
+        # Remove previous automatically created orderpoint that has been refilled (purchase orders).
+
         orderpoints_removed = orderpoints._unlink_processed_orderpoints()
-        orderpoints = orderpoints - orderpoints_removed - to_delete
+        orderpoints = orderpoints - orderpoints_removed
         to_refill = defaultdict(float)
         all_product_ids = []
         all_warehouse_ids = []
-        # Take 3 months since it's the max for the forecast report
-        # Das bedeutet, wenn man den Forcasted Report ausgibt werden nur Produkte angezeigt, deren Bedarfmeldungen innerhalb von 3 Monaten liegen.
-        # Ein MO die heute (31.08.) eröffnet wird, wird noch angezeigt, wenn das geplannte Datum am 30.11. ist, aber verschwindet, wenn das Datum der 1.12 ist.
-        # EDIT: Hanning Liu months von 3 auf 24 gesetzt um die Leadtage zu erhöhen
-        to_date = add(fields.date.today(), months=24)
-        # report.story.quantity zeigt den forcasted reported zu allen Produkten an und ist somit eine Erweiterung zum stock.quant
 
+        allStorables= self.env['product.product'].search([('detailed_type','=','product')])
 
-        #das Grundproblem ist, der forecast ist keine abfrage relaer werte zundern eine geschätzte menge
+        for p in allStorables:
+            qty_INV = p.with_context({'location' : 21}).qty_to_order
+            if qty_INV < 0.0:
+                warehouse_id = 2
+                all_product_ids.append(p.id)
+                all_warehouse_ids.append(warehouse_id)
+                to_refill[(p.id, warehouse_id)] = qty_INV
+            qty_WH = p.with_context({'location' : 8}).qty_to_order
+            if qty_WH < 0.0:
+                warehouse_id = 1
+                all_product_ids.append(p.id)
+                all_warehouse_ids.append(warehouse_id)
+                to_refill[(p.id, warehouse_id)] = qty_WH
+        #    if p.id==46858:
+            #    print("\n#\n#\n#\n#\n#1\n{} {}".format(qty_INV,qty_WH))
 
-
-
-        qty_by_product_warehouse = self.env['bbi.report.stock.quantity'].read_group(
-            #edited tk: auch hier den filter auf date komplett entfernt
-            [('state', '=', 'forecast')],
-            ['product_id', 'product_qty', 'warehouse_id'],
-            ['product_id', 'warehouse_id'], lazy=False)
-
-        #print(len(qty_by_product_warehouse))
-        #print(qty_by_product_warehouse[0])
-        print(len(allStorablesWHStock = self.env['product.product'].with_context(location=8).search([('detailed_type','=','product')])))
-        print(len(allStorablesINVStock = self.env['product.product'].with_context(location=21).search([('detailed_type','=','product')])))
-
-        return
-
-        for item in qty_by_product_warehouse:
-            if item['product_id'][0] == 77776:
-                print('\n#\n#\n#\n1')
-                print(item)
-
-                #todo 2 eigene sql abfragen WH/Stock
-                #warehouse 1 zugänge, location_dest_id = 8 group by product_id sum(qty)
-                #warehouse 1 abgänge, location_id = 8 group by product_id sum(qty)
-                #dif ist menge zum füllen
-
-                #WH/INV
-                #warehouse 2 zugänge, location_dest_id = 21 group by product_id sum(qty)
-                #warehouse 2 abgänge, location_id = 21 group by product_id sum(qty)
-                #dif ist menge zum füllen
-
-
-
-        for group in qty_by_product_warehouse:
-
-        #    if p.virtual_available < 0.0:
-
-            #setzt Warehouse id Stock = 1 Inventar = 2
-            #    if p.warehouse_id
-            warehouse_id = group.get('warehouse_id') and group['warehouse_id'][0]
-            # filtert den Forecast nach Produkte mit einer Prognose unter 0
-            if group['product_qty'] >= 0.0 or not warehouse_id:
-                continue
-            # alle Produkte mit einer Prognose über 0 werden in all_product_ids abgespeichert
-            all_product_ids.append(group['product_id'][0])
-            all_warehouse_ids.append(warehouse_id)
-            # to refill = alle aufzufüllenden Produkte mit dem Format (73628, 1): -5.0 / nur negative Product_qty durch vorherige Abfrage
-            to_refill[(group['product_id'][0], warehouse_id)] = group['product_qty']
         # wenn keine Produkte zum auffüllen vorhanden sind bricht die Methode hier ab
+#
+    #    if not to_refill:
+    #        return action
 
-        if not to_refill:
-            return self.restore_orderpoint_info(std_supplier)
+    #    if  46858 in    all_product_ids:
+    #        print("\n#\n#\n#\n#\n#2 is da\n")
 
-        # Recompute the forecasted quantity for missing product today but at this time
-        # with their real lead days.
-        # Berechnung gruppiert nach leaddays
-        #nachtrag tilo: ich denke hier werden die lead_days betrachtet und am ende des blocks weitere
-        #einträge entfernt,
-        #da wir mal versuchen wollen, die Auffüllung unabhängig von den lead days zu gestalten, wird die zeile unten auskommentiert
-        key_to_remove = []
-        pwh_per_day = defaultdict(list)
-        for (product, warehouse) in to_refill.keys():
-            product = self.env['product.product'].browse(product).with_prefetch(all_product_ids)
-            warehouse = self.env['stock.warehouse'].browse(warehouse).with_prefetch(all_warehouse_ids)
-            # Rules gibt die 'route_ids' und die 'warehouse_id' an
-            rules = product._get_rules_from_location(warehouse.lot_stock_id)
-            # Die Leaddays werden von den Supplierinfo entnommen
-            lead_days = rules._get_lead_days(product)[0]
-            #lead_days = rules.with_context(bypass_delay_description=True)._get_lead_days(product)[0]
-            # EDIT Hanning Liu: Wenn keine Leaddays in der Supplierinfo eingetragen wird, wird derzeit standardmäßig eine Leadtime von 0 eingetragen
-            # Somit wird, sobald die Leaddays 0 betragen automatisch eine 365 eingetragen
-            # Das ist wichtig für die Fertigung, die lediglich einen Bedarf eintragen, aber keinen Lieferanten, wodurch eine Leadtime von 0 entsteht und somit nicht in der Auffüllung angezeigt wird.
-            #if lead_days == 0:
-            #    lead_days = 365
-            #print("Produkt: " + str(product.product_tmpl_id.name) + " Lead_days: " +str(lead_days))
-            #pwh_per_day ist ein Gruppierung der Produkte nach den Leadtagen. D.h. 10 Leadtage --> xxx, xxx, xxx PRoduct
-            #overwrite TK leadtimes künstlich hochsetzen, damit auffüllungsliste alles enthält, was offen ist
-            pwh_per_day[(lead_days, warehouse)].append(product.id)
 
-        #print (str(pwh_per_day))
 
-        # group product by lead_days and warehouse in order to read virtual_available
-        # in batch
-        for (days, warehouse), p_ids in pwh_per_day.items():
-            products = self.env['product.product'].browse(p_ids)
-            qties = products.with_context(
-                warehouse=warehouse.id,
-                to_date=fields.datetime.now() + relativedelta.relativedelta(days=1460) # Mod TK: hier statisch alle zugänge über 4 jahre verrechnen
-            ).read(['virtual_available'])
-
-            #print(str(qties) + " test")
-            for qty in qties:
-                if float_compare(qty['virtual_available'], 0, precision_rounding=product.uom_id.rounding) >= 0:
-                    key_to_remove.append((qty['id'], warehouse.id))
-                else:
-                    to_refill[(qty['id'], warehouse.id)] = qty['virtual_available']
-
-        for key in key_to_remove:  #Auffüllung sollte damit nicht mehr auf die lead times achten
-            del to_refill[key]
-        if not to_refill:
-            return self.restore_orderpoint_info(std_supplier)
 
         # Remove incoming quantity from other origin than moves (e.g RFQ)
         product_ids, warehouse_ids = zip(*to_refill)
+        print("\n#\n#\n#\n#\n#3")
+        print(product_ids)
+        print(warehouse_ids)
         dummy, qty_by_product_wh = self.env['product.product'].browse(product_ids)._get_quantity_in_progress(warehouse_ids=warehouse_ids)
         rounding = self.env['decimal.precision'].precision_get('Product Unit of Measure')
         # Group orderpoint by product-warehouse
@@ -511,6 +420,7 @@ class StockWarehouseOrderpoint(models.Model):
             to_refill[(product, warehouse)] = product_qty + qty_in_progress
         to_refill = {k: v for k, v in to_refill.items() if float_compare(
             v, 0.0, precision_digits=rounding) < 0.0}
+        print("\n#\n#\n#\n#\n#3\n{}".format(to_refill))
 
         lot_stock_id_by_warehouse = self.env['stock.warehouse'].with_context(active_test=False).search_read([
             ('id', 'in', [g[1] for g in to_refill.keys()])
@@ -549,4 +459,4 @@ class StockWarehouseOrderpoint(models.Model):
             if not orderpoint.route_id:
                 orderpoint._set_default_route_id()
             orderpoint.qty_multiple = orderpoint._get_qty_multiple_to_order()
-        return self.restore_orderpoint_info(std_supplier)
+        return action
