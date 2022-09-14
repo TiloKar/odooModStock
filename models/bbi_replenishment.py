936d8a0844fd6dab5bf249530e6898b7efd4b11a
Tilo <tilo.karczewski@gmx>
2022-09-14 17:39:34 +0200

    verkleinert, aufgeräumt, lauffähig mit fehlenden produkten

--- models/bbi_replenishment.py
+++ models/bbi_replenishment.py
@@ -20,155 +20,9 @@ class BbiReportStockQuantityMini(models.Model):
     _auto = False
     _description = 'Stock Quantity Report bbi mod for Replenishment minfied for wh/stock und INV/stock'
 
-    date = fields.Date(string='Date', readonly=True)
-    product_tmpl_id = fields.Many2one('product.template', readonly=True)
-    product_id = fields.Many2one('product.product', string='Product', readonly=True)
-    state = fields.Selection([
-        ('forecast', 'Forecasted Stock'),
-        ('in', 'Forecasted Receipts'),
-        ('out', 'Forecasted Deliveries'),
-    ], string='State', readonly=True)
-    product_qty = fields.Float(string='Quantity', readonly=True)
-    move_ids = fields.One2many('stock.move', readonly=True) #wird nicht benutzt
-    company_id = fields.Many2one('res.company', readonly=True)
-    warehouse_id = fields.Many2one('stock.warehouse', readonly=True)
-
     def init(self):
-        """
-        Because we can transfer a product from a warehouse to another one thanks to a stock move, we need to
-        generate some fake stock moves before processing all of them. That way, in case of an interwarehouse
-        transfer, we will have an outgoing stock move for the source warehouse and an incoming stock move
-        for the destination one. To do so, we select all relevant SM (incoming, outgoing and interwarehouse),
-        then we duplicate all these SM and edit the values:
-            - product_qty is kept if the SM is not the duplicated one or if the SM is an interwarehouse one
-                otherwise, we set the value to 0 (this allows us to filter it out during the SM processing)
-            - the source warehouse is kept if the SM is not the duplicated one
-            - the dest warehouse is kept if the SM is not the duplicated one and is not an interwarehouse
-                OR the SM is the duplicated one and is an interwarehouse
-        """
-        tools.drop_view_if_exists(self._cr, 'bbi_report_stock_quantity')
-        query = """
-CREATE or REPLACE VIEW bbi_report_stock_quantity AS (
-WITH
-    existing_sm (id, product_id, tmpl_id, product_qty, date, state, company_id, whs_id, whd_id) AS (
-        SELECT m.id, m.product_id, pt.id, m.product_qty, m.date, m.state, m.company_id, whs.id, whd.id
-        FROM stock_move m
-        LEFT JOIN stock_location ls on (ls.id=m.location_id)
-        LEFT JOIN stock_location ld on (ld.id=m.location_dest_id)
-        LEFT JOIN stock_warehouse whs ON ls.parent_path like concat('%/', whs.view_location_id, '/%')
-        LEFT JOIN stock_warehouse whd ON ld.parent_path like concat('%/', whd.view_location_id, '/%')
-        LEFT JOIN product_product pp on pp.id=m.product_id
-        LEFT JOIN product_template pt on pt.id=pp.product_tmpl_id
-        WHERE pt.type = 'product' AND
-            (whs.id IS NOT NULL OR whd.id IS NOT NULL) AND
-            (whs.id IS NULL OR whd.id IS NULL OR whs.id != whd.id) AND
-            m.product_qty != 0 AND
-            m.state NOT IN ('draft', 'cancel', 'done')
-    ),
-    all_sm (id, product_id, tmpl_id, product_qty, date, state, company_id, whs_id, whd_id) AS (
-        SELECT sm.id, sm.product_id, sm.tmpl_id,
-            CASE
-                WHEN is_duplicated = 0 THEN sm.product_qty
-                WHEN sm.whs_id IS NOT NULL AND sm.whd_id IS NOT NULL AND sm.whs_id != sm.whd_id THEN sm.product_qty
-                ELSE 0
-            END,
-            sm.date, sm.state, sm.company_id,
-            CASE WHEN is_duplicated = 0 THEN sm.whs_id END,
-            CASE
-                WHEN is_duplicated = 0 AND NOT (sm.whs_id IS NOT NULL AND sm.whd_id IS NOT NULL AND sm.whs_id != sm.whd_id) THEN sm.whd_id
-                WHEN is_duplicated = 1 AND (sm.whs_id IS NOT NULL AND sm.whd_id IS NOT NULL AND sm.whs_id != sm.whd_id) THEN sm.whd_id
-            END
-        FROM
-            GENERATE_SERIES(0, 1, 1) is_duplicated,
-            existing_sm sm
-    )
-SELECT
-    MIN(id) as id,
-    product_id,
-    product_tmpl_id,
-    state,
-    date,
-    sum(product_qty) as product_qty,
-    company_id,
-    warehouse_id
-FROM (SELECT
-        m.id,
-        m.product_id,
-        m.tmpl_id as product_tmpl_id,
-        CASE
-            WHEN m.whs_id IS NOT NULL AND m.whd_id IS NULL THEN 'out'
-            WHEN m.whd_id IS NOT NULL AND m.whs_id IS NULL THEN 'in'
-        END AS state,
-        m.date::date AS date,
-        CASE
-            WHEN m.whs_id IS NOT NULL AND m.whd_id IS NULL THEN -m.product_qty
-            WHEN m.whd_id IS NOT NULL AND m.whs_id IS NULL THEN m.product_qty
-        END AS product_qty,
-        m.company_id,
-        CASE
-            WHEN m.whs_id IS NOT NULL AND m.whd_id IS NULL THEN m.whs_id
-            WHEN m.whd_id IS NOT NULL AND m.whs_id IS NULL THEN m.whd_id
-        END AS warehouse_id
-    FROM
-        all_sm m
-    WHERE
-        m.product_qty != 0 AND
-        m.state != 'done'
-    UNION ALL
-    SELECT
-        -q.id as id,
-        q.product_id,
-        pp.product_tmpl_id,
-        'forecast' as state,
-        date.*::date,
-        q.quantity as product_qty,
-        q.company_id,
-        wh.id as warehouse_id
-    FROM
-        GENERATE_SERIES((now() at time zone 'utc')::date - interval '3month',
-        (now() at time zone 'utc')::date + interval '3 month', '1 day'::interval) date,
-        stock_quant q
-    LEFT JOIN stock_location l on (l.id=q.location_id)
-    LEFT JOIN stock_warehouse wh ON l.parent_path like concat('%/', wh.view_location_id, '/%')
-    LEFT JOIN product_product pp on pp.id=q.product_id
-    WHERE
-        (l.usage = 'internal' AND wh.id IS NOT NULL) OR
-        l.usage = 'transit'
-    UNION ALL
-    SELECT
-        m.id,
-        m.product_id,
-        m.tmpl_id as product_tmpl_id,
-        'forecast' as state,
-        GENERATE_SERIES(
-        CASE
-            WHEN m.state = 'done' THEN (now() at time zone 'utc')::date - interval '3month'
-            ELSE m.date::date
-        END,
-        CASE
-            WHEN m.state != 'done' THEN (now() at time zone 'utc')::date + interval '3 month'
-            ELSE m.date::date - interval '1 day'
-        END, '1 day'::interval)::date date,
-        CASE
-            WHEN m.whs_id IS NOT NULL AND m.whd_id IS NULL AND m.state = 'done' THEN m.product_qty
-            WHEN m.whd_id IS NOT NULL AND m.whs_id IS NULL AND m.state = 'done' THEN -m.product_qty
-            WHEN m.whs_id IS NOT NULL AND m.whd_id IS NULL THEN -m.product_qty
-            WHEN m.whd_id IS NOT NULL AND m.whs_id IS NULL THEN m.product_qty
-        END AS product_qty,
-        m.company_id,
-        CASE
-            WHEN m.whs_id IS NOT NULL AND m.whd_id IS NULL THEN m.whs_id
-            WHEN m.whd_id IS NOT NULL AND m.whs_id IS NULL THEN m.whd_id
-        END AS warehouse_id
-    FROM
-        all_sm m
-    WHERE
-        m.product_qty != 0) AS forecast_qty
-GROUP BY product_id, product_tmpl_id, state, date, company_id, warehouse_id
-);
-"""
-        self.env.cr.execute(query)
-
+        tools.drop_view_if_exists(self._cr, 'bbi_report_stock_quantity_mini')
+        #sicherheitshalber um bereinigen
 
 #eigen kopie des reports für auffüllungs-query mod
 #im ersten select, die bedingung für date komplet tentfernt, date wird noch gebildet, aber nicht mehr zum filtern genutzt
@@ -177,181 +31,24 @@ class BbiReportStockQuantity(models.Model):
     _auto = False
     _description = 'Stock Quantity Report bbi mod for Replenishment'
 
-    date = fields.Date(string='Date', readonly=True)
-    product_tmpl_id = fields.Many2one('product.template', readonly=True)
-    product_id = fields.Many2one('product.product', string='Product', readonly=True)
-    state = fields.Selection([
-        ('forecast', 'Forecasted Stock'),
-        ('in', 'Forecasted Receipts'),
-        ('out', 'Forecasted Deliveries'),
-    ], string='State', readonly=True)
-    product_qty = fields.Float(string='Quantity', readonly=True)
-    move_ids = fields.One2many('stock.move', readonly=True) #wird nicht benutzt
-    company_id = fields.Many2one('res.company', readonly=True)
-    warehouse_id = fields.Many2one('stock.warehouse', readonly=True)
 
     def init(self):
-        """
-        Because we can transfer a product from a warehouse to another one thanks to a stock move, we need to
-        generate some fake stock moves before processing all of them. That way, in case of an interwarehouse
-        transfer, we will have an outgoing stock move for the source warehouse and an incoming stock move
-        for the destination one. To do so, we select all relevant SM (incoming, outgoing and interwarehouse),
-        then we duplicate all these SM and edit the values:
-            - product_qty is kept if the SM is not the duplicated one or if the SM is an interwarehouse one
-                otherwise, we set the value to 0 (this allows us to filter it out during the SM processing)
-            - the source warehouse is kept if the SM is not the duplicated one
-            - the dest warehouse is kept if the SM is not the duplicated one and is not an interwarehouse
-                OR the SM is the duplicated one and is an interwarehouse
-        """
+        #sicherheitshalber um bereinigen
         tools.drop_view_if_exists(self._cr, 'bbi_report_stock_quantity')
-        query = """
-CREATE or REPLACE VIEW bbi_report_stock_quantity AS (
-WITH
-    existing_sm (id, product_id, tmpl_id, product_qty, date, state, company_id, whs_id, whd_id) AS (
-        SELECT m.id, m.product_id, pt.id, m.product_qty, m.date, m.state, m.company_id, whs.id, whd.id
-        FROM stock_move m
-        LEFT JOIN stock_location ls on (ls.id=m.location_id)
-        LEFT JOIN stock_location ld on (ld.id=m.location_dest_id)
-        LEFT JOIN stock_warehouse whs ON ls.parent_path like concat('%/', whs.view_location_id, '/%')
-        LEFT JOIN stock_warehouse whd ON ld.parent_path like concat('%/', whd.view_location_id, '/%')
-        LEFT JOIN product_product pp on pp.id=m.product_id
-        LEFT JOIN product_template pt on pt.id=pp.product_tmpl_id
-        WHERE pt.type = 'product' AND
-            (whs.id IS NOT NULL OR whd.id IS NOT NULL) AND
-            (whs.id IS NULL OR whd.id IS NULL OR whs.id != whd.id) AND
-            m.product_qty != 0 AND
-            m.state NOT IN ('draft', 'cancel', 'done')
-    ),
-    all_sm (id, product_id, tmpl_id, product_qty, date, state, company_id, whs_id, whd_id) AS (
-        SELECT sm.id, sm.product_id, sm.tmpl_id,
-            CASE
-                WHEN is_duplicated = 0 THEN sm.product_qty
-                WHEN sm.whs_id IS NOT NULL AND sm.whd_id IS NOT NULL AND sm.whs_id != sm.whd_id THEN sm.product_qty
-                ELSE 0
-            END,
-            sm.date, sm.state, sm.company_id,
-            CASE WHEN is_duplicated = 0 THEN sm.whs_id END,
-            CASE
-                WHEN is_duplicated = 0 AND NOT (sm.whs_id IS NOT NULL AND sm.whd_id IS NOT NULL AND sm.whs_id != sm.whd_id) THEN sm.whd_id
-                WHEN is_duplicated = 1 AND (sm.whs_id IS NOT NULL AND sm.whd_id IS NOT NULL AND sm.whs_id != sm.whd_id) THEN sm.whd_id
-            END
-        FROM
-            GENERATE_SERIES(0, 1, 1) is_duplicated,
-            existing_sm sm
-    )
-SELECT
-    MIN(id) as id,
-    product_id,
-    product_tmpl_id,
-    state,
-    date,
-    sum(product_qty) as product_qty,
-    company_id,
-    warehouse_id
-FROM (SELECT
-        m.id,
-        m.product_id,
-        m.tmpl_id as product_tmpl_id,
-        CASE
-            WHEN m.whs_id IS NOT NULL AND m.whd_id IS NULL THEN 'out'
-            WHEN m.whd_id IS NOT NULL AND m.whs_id IS NULL THEN 'in'
-        END AS state,
-        m.date::date AS date,
-        CASE
-            WHEN m.whs_id IS NOT NULL AND m.whd_id IS NULL THEN -m.product_qty
-            WHEN m.whd_id IS NOT NULL AND m.whs_id IS NULL THEN m.product_qty
-        END AS product_qty,
-        m.company_id,
-        CASE
-            WHEN m.whs_id IS NOT NULL AND m.whd_id IS NULL THEN m.whs_id
-            WHEN m.whd_id IS NOT NULL AND m.whs_id IS NULL THEN m.whd_id
-        END AS warehouse_id
-    FROM
-        all_sm m
-    WHERE
-        m.product_qty != 0 AND
-        m.state != 'done'
-    UNION ALL
-    SELECT
-        -q.id as id,
-        q.product_id,
-        pp.product_tmpl_id,
-        'forecast' as state,
-        date.*::date,
-        q.quantity as product_qty,
-        q.company_id,
-        wh.id as warehouse_id
-    FROM
-        GENERATE_SERIES((now() at time zone 'utc')::date - interval '3month',
-        (now() at time zone 'utc')::date + interval '3 month', '1 day'::interval) date,
-        stock_quant q
-    LEFT JOIN stock_location l on (l.id=q.location_id)
-    LEFT JOIN stock_warehouse wh ON l.parent_path like concat('%/', wh.view_location_id, '/%')
-    LEFT JOIN product_product pp on pp.id=q.product_id
-    WHERE
-        (l.usage = 'internal' AND wh.id IS NOT NULL) OR
-        l.usage = 'transit'
-    UNION ALL
-    SELECT
-        m.id,
-        m.product_id,
-        m.tmpl_id as product_tmpl_id,
-        'forecast' as state,
-        GENERATE_SERIES(
-        CASE
-            WHEN m.state = 'done' THEN (now() at time zone 'utc')::date - interval '3month'
-            ELSE m.date::date
-        END,
-        CASE
-            WHEN m.state != 'done' THEN (now() at time zone 'utc')::date + interval '3 month'
-            ELSE m.date::date - interval '1 day'
-        END, '1 day'::interval)::date date,
-        CASE
-            WHEN m.whs_id IS NOT NULL AND m.whd_id IS NULL AND m.state = 'done' THEN m.product_qty
-            WHEN m.whd_id IS NOT NULL AND m.whs_id IS NULL AND m.state = 'done' THEN -m.product_qty
-            WHEN m.whs_id IS NOT NULL AND m.whd_id IS NULL THEN -m.product_qty
-            WHEN m.whd_id IS NOT NULL AND m.whs_id IS NULL THEN m.product_qty
-        END AS product_qty,
-        m.company_id,
-        CASE
-            WHEN m.whs_id IS NOT NULL AND m.whd_id IS NULL THEN m.whs_id
-            WHEN m.whd_id IS NOT NULL AND m.whs_id IS NULL THEN m.whd_id
-        END AS warehouse_id
-    FROM
-        all_sm m
-    WHERE
-        m.product_qty != 0) AS forecast_qty
-GROUP BY product_id, product_tmpl_id, state, date, company_id, warehouse_id
-);
-"""
-        self.env.cr.execute(query)
+
 
 
 class StockWarehouseOrderpoint(models.Model):
     _inherit = "stock.warehouse.orderpoint"
 
-    def restore_orderpoint_info(self,supInfos):
-        action = self.env["ir.actions.actions"]._for_xml_id("stock.action_orderpoint_replenish")
-        action['context'] = self.env.context
-        orderpoints = self.env['stock.warehouse.orderpoint'].search([])
-        for op in orderpoints:
-            for item in supInfos:
-                #print(op.product_id.id)
-                if op.product_id.id == item['product_id']:
-                    op.supplier_id = item['supplier_id']
-                    op.vendor_id = item['vendor_id']
-
-        return action
 
     def _get_orderpoint_action(self):
-        """Create manual orderpoints for missing product in each warehouses. It also removes
-        orderpoints that have been replenish. In order to do it:
-        - It uses the report.stock.quantity to find missing quantity per product/warehouse
-        - It checks if orderpoint already exist to refill this location.
-        - It checks if it exists other sources (e.g RFQ) tha refill the warehouse.
-        - It creates the orderpoints for missing quantity that were not refill by an upper option.
-        return replenish report ir.actions.act_window
-        """
+
+        #probelem: einige Produkte z.B. 46858 tauchen nicht auf
+        #db dump vom 14.09 dazu liegt unter  Z:\Austausch\TK
+
+
+
         #eigentliche Rückgabe zum Aufbau im Frontend, wird durch manipulation auf variable orderpoints im laufeder methode verändert
         action = self.env["ir.actions.actions"]._for_xml_id("stock.action_orderpoint_replenish")
         action['context'] = self.env.context
@@ -369,13 +66,13 @@ class StockWarehouseOrderpoint(models.Model):
         allStorables= self.env['product.product'].search([('detailed_type','=','product')])
 
         for p in allStorables:
-            qty_INV = p.with_context({'location' : 21}).qty_to_order
+            qty_INV = p.with_context({'location' : 21}).virtual_available
             if qty_INV < 0.0:
                 warehouse_id = 2
                 all_product_ids.append(p.id)
                 all_warehouse_ids.append(warehouse_id)
                 to_refill[(p.id, warehouse_id)] = qty_INV
-            qty_WH = p.with_context({'location' : 8}).qty_to_order
+            qty_WH = p.with_context({'location' : 8}).virtual_available
             if qty_WH < 0.0:
                 warehouse_id = 1
                 all_product_ids.append(p.id)
